c9e8c9ab7a6dff7430258773c5a5f1666d89db75
Erin La <107987318+giterinhub@users.noreply.github.com>
2024-05-15 21:13:00 +0000

    uplimit week 1

---
+++ edge_server/build.sh
@@ -0,0 +1,3 @@
+docker build -t edge_server:v1 .
+# docker build edge_server -t v1 . 
+# docker run -p 8000:80 edge_server
---
+++ edge_server/deploy.sh
@@ -0,0 +1,1 @@
+docker run -d -p 8000:80 --restart=on-failure edge_server:v1 
